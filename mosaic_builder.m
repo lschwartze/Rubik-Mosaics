2d41542207f66e43f6c396e0cad2fb6a6a331900
Laurin Schwartze <victor.schwartze@gmail.com>
2023-08-04 15:12:04 +0200

    initial commit

---
+++ mosaic_builder.m
@@ -0,0 +1,9 @@
+MAX_WIDTH = 30;
+WHITE = [255, 255, 255];
+YELLOW = [0, 255, 255];
+GREEN = [0, 255, 0];
+BLUE = [0, 0, 255];
+RED = [255, 0, 0];
+ORANGE = [255, 165, 0];
+
+[file, path] = uigetfile('*.png');
